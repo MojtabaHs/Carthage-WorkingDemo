ce0290331e4acda59f044a4dbc3d1e39ffe1d98d
Seyed Mojtaba Hosseini Zeidabadi <hosseini@chenzook.com>
2018-11-06 09:48:08 +0330

    Initial Commit

---
+++ StackOverflow-52987843/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StackOverflow-52987843
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 11/6/18.
+//  Copyright © 2018 Chenzook. All rights reserved.
+//
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//  Web: https://chenzook.com
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
